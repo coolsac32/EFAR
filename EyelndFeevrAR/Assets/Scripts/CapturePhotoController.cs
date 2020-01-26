a695a95122b737feeb08bc37ed9ee29a40415712
coolsac32 <51429905+coolsac32@users.noreply.github.com>
2020-01-26 02:57:27 -0800

    added snapshot mode to photobooth

---
+++ EyelndFeevrAR/Assets/Scripts/CapturePhotoController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class CapturePhotoController : MonoBehaviour
+{
+    public CaptureAndSave captureAndSave;
+    public GameObject uiToBeHidden;
+
+    private void OnEnable()
+    {
+        CaptureAndSaveEventListener.onSuccess += SuccessCapturePhoto;
+        CaptureAndSaveEventListener.onError += FailCapturePhoto;
+    }
+
+    private void OnDisable()
+    {
+        CaptureAndSaveEventListener.onSuccess -= SuccessCapturePhoto;
+        CaptureAndSaveEventListener.onError -= FailCapturePhoto;
+    }
+
+    public void CapturePhoto()
+    {
+        uiToBeHidden.SetActive(false);
+        captureAndSave.CaptureAndSaveToAlbum();
+    }
+
+    private void SuccessCapturePhoto(string msg)
+    {
+        AndroidNativeFunctions.ShowToast("Photo Saved to Gallery");
+        uiToBeHidden.SetActive(true);
+    }
+
+    private void FailCapturePhoto(string msg)
+    {
+        AndroidNativeFunctions.ShowToast(msg);
+        uiToBeHidden.SetActive(true);
+    }
+}
---
+++ EyelndFeevrAR/Assets/Scripts/CapturePhotoControllerIOS.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class CapturePhotoControllerIOS : MonoBehaviour
+{
+    public CaptureAndSave captureAndSave;
+    public GameObject uiToBeHidden;
+
+    private void OnEnable()
+    {
+        CaptureAndSaveEventListener.onSuccess += SuccessCapturePhoto;
+        CaptureAndSaveEventListener.onError += FailCapturePhoto;
+    }
+
+    private void OnDisable()
+    {
+        CaptureAndSaveEventListener.onSuccess -= SuccessCapturePhoto;
+        CaptureAndSaveEventListener.onError -= FailCapturePhoto;
+    }
+
+    public void CapturePhoto()
+    {
+        uiToBeHidden.SetActive(false);
+        captureAndSave.CaptureAndSaveToAlbum();
+    }
+
+    private void SuccessCapturePhoto(string msg)
+    {
+        //Give Success message
+        uiToBeHidden.SetActive(true);
+    }
+
+    private void FailCapturePhoto(string msg)
+    {
+        //Give fail message
+        uiToBeHidden.SetActive(true);
+    }
+}
