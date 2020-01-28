593052b37859038e50e10f7cd7bd34befc30ad20
coolsac32 <51429905+coolsac32@users.noreply.github.com>
2020-01-27 22:28:16 -0800

    Added Audio Manager Function

---
+++ EyelndFeevrAR/Assets/Scripts/Cover1RecoScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class Cover1RecoScript : MonoBehaviour, ITrackableEventHandler
+{
+    public AudioClip musicFx;
+
+    private TrackableBehaviour mTrackableBehaviour;
+
+    void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+
+    public void OnTrackableStateChanged(
+                                    TrackableBehaviour.Status previousStatus,
+                                    TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            //lancio la musica
+            Debug.Log("sound");
+            AudioManagerScript.current.PlaySound(musicFx);
+        }
+        else
+        {
+            // stoppo la musica
+            AudioManagerScript.current.StopSound();
+            //  Debug.Log("stoppo");
+
+        }
+    }
+}
