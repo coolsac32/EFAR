85ef850e84724fb3a44c05b56346173b67b23cc9
coolsac32 <51429905+coolsac32@users.noreply.github.com>
2020-01-29 08:44:25 -0800

    Added puase/play button for animation in canvas

---
+++ EyelndFeevrAR/Assets/Scripts/PlayPauseManagerScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayPauseManagerScript : MonoBehaviour
+{
+    public Animator[] myAnimator;
+    public Animation[] animationComponents;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnimator = GetComponentsInChildren<Animator>();
+        animationComponents = GetComponentsInChildren<Animation>();
+    }
+
+
+    public void pauseAnim()
+    {
+        // *** Start of animation code
+
+        foreach (Animation animation in animationComponents)
+        {
+            foreach (AnimationState animState in animation)
+            {
+                animState.speed = 0f;
+
+            }
+        }
+
+        // *** end of animation code
+
+        foreach (Animator animator in myAnimator)
+        {
+            animator.speed = 0f;
+        }
+    }
+
+    public void resumeAnim()
+    {
+
+        // *** Start of animation code
+
+        foreach (Animation animation in animationComponents)
+        {
+            foreach (AnimationState animState in animation)
+            {
+                animState.speed = 1f;
+                animation.Play();
+            }
+        }
+
+        foreach (Animator animator in myAnimator)
+        {
+            animator.speed = 1f;
+        }
+
+        // *** end of animation code
+    }
+}
+
+
