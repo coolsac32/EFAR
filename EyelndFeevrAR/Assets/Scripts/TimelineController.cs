c1f919e3452f18f789e48b11f2ab5f6ea269ac5f
coolsac32 <51429905+coolsac32@users.noreply.github.com>
2020-02-18 15:34:45 -0800

    Added iOS Camera function and Audio Book Controls

---
+++ EyelndFeevrAR/Assets/Scripts/TimelineController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelineController : MonoBehaviour
+{
+    public PlayableDirector playableDirector;
+
+    // Start is called before the first frame update
+
+    public void Play()
+    {
+        playableDirector.Play();
+    }
+
+    public void Resume()
+    {
+        playableDirector.Resume();
+    }
+
+    public void Pause()
+    {
+        playableDirector.Pause();
+    }
+}
