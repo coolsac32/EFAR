f8ac263e136e45c89396ebc1b7f4dca2d7bc43d5
coolsac32 <51429905+coolsac32@users.noreply.github.com>
2020-02-22 10:25:27 -0800

    Updated Volume Control and Firebase support

---
+++ EyelndFeevrAR/Assets/Scripts/FirebaseNotificationController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FirebaseNotificationController : MonoBehaviour
+{
+
+    public Text deviceToken;
+
+    public void Start()
+    {
+        Firebase.Messaging.FirebaseMessaging.TokenReceived += OnTokenReceived;
+        Firebase.Messaging.FirebaseMessaging.MessageReceived += OnMessageReceived;
+    }
+
+    public void OnTokenReceived(object sender, Firebase.Messaging.TokenReceivedEventArgs token)
+    {
+        Debug.Log("Received Registration Token: " + token.Token);
+        deviceToken.text = token.Token;
+    }
+
+    public void OnMessageReceived(object sender, Firebase.Messaging.MessageReceivedEventArgs e)
+    {
+        UnityEngine.Debug.Log("From: " + e.Message.From);
+        UnityEngine.Debug.Log("Message ID: " + e.Message.MessageId);
+    }
+}
